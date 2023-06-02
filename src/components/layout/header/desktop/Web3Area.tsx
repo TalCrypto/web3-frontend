91004c40d37491315caed710ef6d459149ef68c3
Edward <edward@tribe3.xyz>
2023-06-02 16:07:45 +0900

    fix: add the wallet address balance

--- src/components/layout/header/desktop/Web3Area.tsx
+++ src/components/layout/header/desktop/Web3Area.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { useStore as useNanostore } from '@nanostores/react';
 import { ThreeDots } from 'react-loader-spinner';
@@ -22,9 +23,17 @@ import {
   wsIsWalletLoading
 } from '@/stores/WalletState';
 
-import { setIsWhitelisted, setIsTethCollected } from '@/stores/UserState';
+import {
+  setIsWhitelisted,
+  setIsTethCollected,
+  setIsDataFetch,
+  setIsHasPartialClose,
+  setIsHasTraded,
+  setIsInputCode
+} from '@/stores/UserState';
 
 import { localeConversion } from '@/utils/localeConversion';
+import { apiConnection } from '@/utils/apiConnection';
 
 import ConnectWalletButton from './ConnectWalletButton';
 import ExtraComponent from './ExtraComponent';
@@ -117,32 +126,93 @@ function Web3Area() {
     });
   }, []);
 
+  useEffect(() => {
+    const holderAddr = walletProvider.holderAddress;
+    if (holderAddr) {
+      // walletProvider.getUserCollectionsInfo(walletProvider.holderAddress).then(userPosition => {
+      //   const portfolio = userPosition.reduce(
+      //     (pre: any, item: any) => (!item ? Number(pre) + 0 : Number(pre) + Number(calculateNumber(item.realMargin, 4))),
+      //     0
+      //   );
+      //   setCallBalance({
+      //     portfolio,
+      //     available: walletProvider.wethBalance
+      //   });
+      // });
+      apiConnection.getUserInfo(holderAddr).then(result => {
+        setUserInfo(result.data);
+      });
+    }
+
+    if (walletProvider.holderAddress) {
+      // new initial data
+      // getInitialData();
+
+      walletProvider.isDataFetch = true;
+      setIsDataFetch(true); // userState store
+      fetchUserData()
+        .then(val => {
+          setIsDataFetched(val); // local state
+          setIsDataFetch(false); // userState store
+          walletProvider.isDataFetch = false;
+          wsIsWalletLoading.set(false);
+        })
+        .catch(() => {
+          setIsDataFetch(false);
+          walletProvider.isDataFetch = false;
+          wsIsWalletLoading.set(false);
+        });
+
+      // check if user has partial close
+      apiConnection.checkUserHasPartialClose().then((res: any) => {
+        const { hasPartialClose } = res.data;
+        setIsHasPartialClose(hasPartialClose);
+      });
+
+      // check if user has traded once
+      if (walletProvider.firebaseIdToken) {
+        apiConnection.validateUserTradingState(walletProvider.firebaseIdToken).then((res: any) => {
+          setIsHasTraded(res.data?.hasTraded);
+          setIsInputCode(res.data?.isInputCode);
+        });
+      }
+
+      // get user point
+      apiConnection.getUserPoint();
+
+      // // get leaderboard rank
+      // if (router.pathname === '/airdrop') {
+      //   apiConnection.getLeaderboard();
+      // }
+    }
+  }, [walletProvider.holderAddress]);
+
   return (
     <div
       className="navbar-container relative mx-auto flex h-[60px] items-start
         justify-start p-0 py-[14px] text-[16px] font-medium text-white">
-      {/* {!userIsLoginStore ? null : ( */}
-      <Link href="/airdrop" className="hidden md:block">
-        <div
-          className="flex h-[32px] cursor-pointer items-center space-x-[4px] rounded-full
+      {!isLogin ? null : (
+        <Link href="/airdrop" className="hidden md:block">
+          <div
+            className="flex h-[32px] cursor-pointer items-center space-x-[4px] rounded-full
             border-[1px] border-warn px-[16px] py-[6px] text-warn hover:bg-warn/20">
-          {userPointIsLoading ? (
-            <ThreeDots ariaLabel="loading-indicator" height={20} width={50} color="white" />
-          ) : (
-            <>
-              <Image src="/images/components/layout/header/user-point.svg" alt="" width={14} height={14} />
-              <span className="text-[16px] font-[500] leading-[20px]">{eligible() && !isBan ? localeConversion(total) : '0.0'}</span>
-            </>
-          )}
-        </div>
-      </Link>
-      {/* )} */}
+            {userPointIsLoading ? (
+              <ThreeDots ariaLabel="loading-indicator" height={20} width={50} color="white" />
+            ) : (
+              <>
+                <Image src="/images/components/layout/header/user-point.svg" alt="" width={14} height={14} />
+                <span className="text-[16px] font-[500] leading-[20px]">{eligible() && !isBan ? localeConversion(total) : '0.0'}</span>
+              </>
+            )}
+          </div>
+        </Link>
+      )}
 
-      {/* {isDataFetched && isLogin ? ( */}
-      <div className="hidden md:block">
-        <ExtraComponent />
-      </div>
-      {/* ) : null} */}
+      {isDataFetched && isLogin ? (
+        <div className="hidden md:block">
+          <ExtraComponent />
+        </div>
+      ) : null}
 
       <ConnectWalletButton
         isLogin={isLogin}
