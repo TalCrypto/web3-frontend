888bb6794ba50b6858dcde3041e5ed44284249ce
Edward <edward@tribe3.xyz>
2023-06-07 01:14:03 +0900

    fix: parital close modal style

--- src/components/trade/desktop/trading/PartialCloseModal.tsx
+++ src/components/trade/desktop/trading/PartialCloseModal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function PartialCloseModal(props: any) {
-  const { isShow, setIsShow, onClickSubmit, mobile } = props;
+  const { isShow, setIsShow, onClickSubmit } = props;
   if (!isShow) {
     return null;
   }
@@ -12,11 +12,16 @@ export default function PartialCloseModal(props: any) {
   };
 
   return (
-    <div className={`partialclosemodalbg ${mobile ? 'mobile' : ''}`} onClick={dismissModal}>
-      <div className={`partialclosemodal ${mobile ? 'mobile' : ''}`}>
+    <div
+      className={`fixed inset-0 z-10 flex h-screen
+        items-center justify-center overflow-auto bg-black bg-opacity-40`}
+      onClick={dismissModal}>
+      <div
+        className={`relative mx-auto w-[500px] overflow-hidden
+          rounded-[12px] bg-secondaryBlue`}>
         {/* <div className="col headerrow"> */}
-        <div className="col">
-          <div className="col closebuttonrow">
+        <div className="mr-4 pb-1 pt-[10px] text-end">
+          <div className="items-initial flex content-center justify-end">
             <Image
               src="/images/components/common/modal/close.svg"
               alt=""
@@ -33,16 +38,18 @@ export default function PartialCloseModal(props: any) {
             Adjust Collateral Function
           </div> */}
         </div>
-        <div className="contentrow">
-          <div className="desctext">
+        <div className="relative px-16 py-9 text-center text-[16px]">
+          <div className="text-[15px]">
             <p>
               Partially closing a position would NOT release any collateral. Please do so by adjusting collateral, which doesn’t cost any
               transaction fee.
             </p>
           </div>
-          <div className="buttonrow mx-auto">
+          <div className="mt-7">
             <button
-              className="button-submit"
+              className="z-700 partial-button relative min-w-[160px] flex-1
+                rounded-full border-[1px] border-[#3576f7] px-4 py-[3px]
+                text-[14px] text-highEmphasis"
               onClick={e => {
                 e.stopPropagation();
                 onClickSubmit();
@@ -50,7 +57,9 @@ export default function PartialCloseModal(props: any) {
               Continue Close
             </button>
             <button
-              className="button-submit"
+              className="z-700 partial-button relative ml-3 min-w-[160px]
+                flex-1 rounded-full border-[1px] border-[#3576f7] px-4 py-[3px]
+                text-[14px] text-highEmphasis"
               onClick={e => {
                 e.preventDefault();
                 dismissModal();
@@ -58,7 +67,13 @@ export default function PartialCloseModal(props: any) {
               Cancel
             </button>
           </div>
-          <Image src="/images/components/common/modal/modal-logo.svg" width={170} height={165} alt="" className="tribelogos" />
+          <Image
+            src="/images/components/common/modal/modal-logo.svg"
+            width={170}
+            height={165}
+            alt=""
+            className="absolute bottom-0 right-0 mr-3 flex items-end"
+          />
         </div>
       </div>
     </div>
