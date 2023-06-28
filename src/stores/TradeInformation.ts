fbeb92bee8214cb0ed3bfced352cd2ff8749ce9c
TalCrypto <wnick9381@gmail.com>
2023-06-28 11:41:07 +0800

    fix: remove unnecessary stores

--- src/stores/TradeInformation.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'nanostores';
-
-export const tsMarketHistory = atom([]);
-export const tsFundingPaymentHistory = atom([]);
-export const tsSportPriceList = atom([]);
--- src/stores/common.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'nanostores';
-
-export const $isSwitcherFirstRender = atom(true);
-
-export const $marketData = atom([]);
-
-export const $isShowMobileModal = atom(false);
