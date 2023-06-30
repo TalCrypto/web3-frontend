a7aa032bb6c8375b61046056f03397916bd816c9
TalCrypto <wnick9381@gmail.com>
2023-06-30 19:42:37 +0800

    fix: handle server-side error when open new position

--- src/hooks/fpHistory.ts
+++ src/hooks/fpHistory.ts
@@ -26,7 +26,7 @@ export const useFundingPaymentHistory = (amm: AMM) => {
         apiConnection.getUserFundingPaymentHistoryWithAmm(address, ammAddress).then(res => {
           setTotal(formatBigInt(res.data.total));
           setFpRecords(
-            res.data.fundingPaymentPnlHistory
+            Array.isArray(res.data.fundingPaymentPnlHistory)
               ? res.data.fundingPaymentPnlHistory.map((item: { timestamp: number; fundingPaymentPnl: string }) => ({
                   timestamp: Number(item.timestamp),
                   fundingPaymentPnl: formatBigInt(item.fundingPaymentPnl)
