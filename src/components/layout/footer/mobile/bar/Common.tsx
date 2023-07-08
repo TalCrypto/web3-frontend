960b33e4e4c89ad0bf11d1157cede61ae61d9ea5
Edward <edward@tribe3.xyz>
2023-07-09 02:23:49 +0900

    feat: bottom bar image redirecting to the trade page

--- src/components/layout/footer/mobile/bar/Common.tsx
+++ src/components/layout/footer/mobile/bar/Common.tsx
@@ -6,9 +6,20 @@ function MobileCommonFooterInfo() {
   const router = useRouter();
   const title = router.pathname === '/portfolio' ? 'Portfolio' : router.pathname === '/airdrop' ? 'Airdrop' : '';
 
+  const gotoTrade = () => {
+    router.push(`/trade`);
+  };
+
   return (
     <>
-      <Image className="absolute left-[20px]" src="/images/components/portfolio/footer_logo.svg" width={24} height={24} alt="" />
+      <Image
+        onClick={gotoTrade}
+        className="absolute left-[20px]"
+        src="/images/components/portfolio/footer_logo.svg"
+        width={24}
+        height={24}
+        alt=""
+      />
       <div className="w-full pl-[66px] text-center text-[15px] text-highEmphasis">{title}</div>
     </>
   );
