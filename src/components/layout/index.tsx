c7e56db88155a5c0e59e746484ec8dd620bd5af0
agamtribe3 <agam@tribe3.xyz>
2023-07-14 15:07:50 +0700

    add video handler for ios metamask browser

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable operator-linebreak */
-import * as React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { $isMobileView, $isShowMobileModal } from '@/stores/modal';
 import { useStore as useNanostore } from '@nanostores/react';
 import LayoutUpdater from '@/components/updaters/LayoutUpdater';
@@ -25,14 +25,66 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   const competitionActiveDropdown = useNanostore($activeDropdown);
 
+  // competition page bg video
+  const videoRef: any = useRef(null);
+
+  useEffect(() => {
+    const handleUserInteraction = () => {
+      // Play the video when the user interacts with the site
+      if (videoRef.current) {
+        videoRef.playsInline = true;
+        videoRef.current.play();
+      }
+    };
+
+    // Listen for user interaction events (e.g., click, touch, key press)
+    window.addEventListener('mousedown', handleUserInteraction);
+    window.addEventListener('touchstart', handleUserInteraction);
+    window.addEventListener('keydown', handleUserInteraction);
+
+    // Clean up event listeners when the component unmounts
+    return () => {
+      window.removeEventListener('mousedown', handleUserInteraction);
+      window.removeEventListener('touchstart', handleUserInteraction);
+      window.removeEventListener('keydown', handleUserInteraction);
+    };
+  }, []);
+
+  function failed(e: any) {
+    // video playback failed - show a message saying why
+    switch (e.target.error.code) {
+      case e.target.error.MEDIA_ERR_ABORTED:
+        alert('You aborted the video playback.');
+        break;
+      case e.target.error.MEDIA_ERR_NETWORK:
+        alert('A network error caused the video download to fail part-way.');
+        break;
+      case e.target.error.MEDIA_ERR_DECODE:
+        alert(
+          'The video playback was aborted due to a corruption problem or because the video used features your browser did not support.'
+        );
+        break;
+      case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+        alert('The video could not be loaded, either because the server or network failed or because the format is not supported.');
+        break;
+      default:
+        alert('An unknown error occurred.');
+        break;
+    }
+  }
+
   return (
     <>
       <Header />
       {isCompetitionPage ? (
         <video
+          // onError={failed}
+          ref={videoRef}
           autoPlay
+          playsInline
           loop
           muted
+          preload="auto"
           className={`${
             competitionActiveDropdown === 0 ? '' : 'hidden md:block'
           } absolute -top-[12vw] h-[93vw] w-full object-cover md:-top-[109px] md:h-auto md:object-fill`}>
