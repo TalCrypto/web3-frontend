aa03095ac99dbeb06f8128711197a7304591c972
Wong Tsz Hei Ernest <lz5663@hotmail.com.hk>
2023-07-18 18:45:07 +0800

    update loading behaviour

--- src/components/trade/mobile/trading/DisplayCollections.tsx
+++ src/components/trade/mobile/trading/DisplayCollections.tsx
@@ -3,6 +3,7 @@ import { useAccount } from 'wagmi';
 import Image from 'next/image';
 import Router, { useRouter } from 'next/router';
 import { useStore as useNanostore } from '@nanostores/react';
+import { ThreeDots } from 'react-loader-spinner';
 
 import { PriceWithIcon } from '@/components/common/PriceWithIcon';
 import { $isShowMobileModal } from '@/stores/modal';
@@ -25,7 +26,6 @@ export default function DisplayCollections() {
 
   const marketUpdateTrigger = useNanostore($marketUpdateTrigger);
   const updateOverviewData = () => {
-    alert('updateOverviewData');
     $marketUpdateTrigger.set(!marketUpdateTrigger);
   };
 
@@ -50,69 +50,74 @@ export default function DisplayCollections() {
         <div className="mt-[12px] h-[1px] w-full bg-[#2E4371]" />
       </div>
       <div className="flex h-[calc(100%-104px)] flex-col overflow-y-scroll ">
-        {ammList.map((item: any, index) => {
-          const key = `switcher_collection_${index}`;
-          const collectionInfo = getCollectionInformation(item);
-          const tradingDataList: any = overviewData?.filter((dataItem: any) => item === dataItem.amm);
-          const tradingData = tradingDataList?.length > 0 ? tradingDataList[0] : null;
+        {isMarketDataUpdating ? (
+          <div className="flex h-full items-center justify-center">
+            <ThreeDots ariaLabel="loading-indicator" height={60} width={60} color="white" />
+          </div>
+        ) : (
+          ammList.map((item: any, index) => {
+            const key = `switcher_collection_${index}`;
+            const collectionInfo = getCollectionInformation(item);
+            const tradingDataList: any = overviewData?.filter((dataItem: any) => item === dataItem.amm);
+            const tradingData = tradingDataList?.length > 0 ? tradingDataList[0] : null;
 
-          const vAMMPrice = !tradingData ? 0 : tradingData.vammPrice;
-          const oraclePrice = !tradingData ? 0 : tradingData.oraclePrice;
-          const priceGap = vAMMPrice && oraclePrice ? vAMMPrice - oraclePrice : 0;
-          const priceGapPercentage = vAMMPrice && oraclePrice ? (vAMMPrice / oraclePrice - 1) * 100 : 0;
-          const isPriceGapPositive = priceGap > 0;
+            const vAMMPrice = !tradingData ? 0 : tradingData.vammPrice;
+            const oraclePrice = !tradingData ? 0 : tradingData.oraclePrice;
+            const priceGap = vAMMPrice && oraclePrice ? vAMMPrice - oraclePrice : 0;
+            const priceGapPercentage = vAMMPrice && oraclePrice ? (vAMMPrice / oraclePrice - 1) * 100 : 0;
+            const isPriceGapPositive = priceGap > 0;
 
-          const isHasPos = (amm: AMM): boolean => {
-            const size = positionInfos[amm]?.size;
-            if (size) {
-              return size !== 0;
-            }
-            return false;
-          };
+            const isHasPos = (amm: AMM): boolean => {
+              const size = positionInfos[amm]?.size;
+              if (size) {
+                return size !== 0;
+              }
+              return false;
+            };
 
-          return (
-            <div
-              key={key}
-              className=""
-              onClick={() => {
-                router.push(`/trade/${collectionInfo.amm.toLowerCase()}`, undefined, { shallow: true });
-                handleBackClick();
-              }}>
-              <div className="my-[16px] flex flex-col px-[20px]">
-                <div className="flex flex-row items-start justify-between ">
-                  <div className="flex flex-row items-start justify-start text-[14px] font-[600]">
-                    <Image src={collectionInfo.image} width={20} height={20} alt="" className="mr-[6px]" />
-                    {collectionInfo.collectionName}
+            return (
+              <div
+                key={key}
+                className=""
+                onClick={() => {
+                  router.push(`/trade/${collectionInfo.amm.toLowerCase()}`, undefined, { shallow: true });
+                  handleBackClick();
+                }}>
+                <div className="my-[16px] flex flex-col px-[20px]">
+                  <div className="flex flex-row items-start justify-between ">
+                    <div className="flex flex-row items-start justify-start text-[14px] font-[600]">
+                      <Image src={collectionInfo.image} width={20} height={20} alt="" className="mr-[6px]" />
+                      {collectionInfo.collectionName}
+                    </div>
+                    {isHasPos(tradingData?.amm) ? (
+                      <Image src="/images/mobile/pages/trade/shopping-bag-green.svg" width={20} height={20} alt="" />
+                    ) : null}
                   </div>
-                  {isHasPos(tradingData?.amm) ? (
-                    <Image src="/images/mobile/pages/trade/shopping-bag-green.svg" width={20} height={20} alt="" />
-                  ) : null}
-                </div>
-                <div className="mt-[20px] flex flex-row items-start justify-between text-[14px] font-[600]">
-                  <div className="flex flex-row items-start font-[400]">
-                    <PriceWithIcon
-                      priceValue={tradingData && tradingData.vammPrice ? tradingData.vammPrice.toFixed(2) : '-.--'}
-                      className={`${!tradingData || isMarketDataUpdating ? 'flash' : ''} !font-[400]`}
-                    />
-                    &nbsp; / &nbsp;
-                    <PriceWithIcon
-                      priceValue={tradingData && tradingData.oraclePrice ? tradingData.oraclePrice.toFixed(2) : '-.--'}
-                      className={`${!tradingData || isMarketDataUpdating ? 'flash' : ''} !font-[400] !text-mediumEmphasis`}
-                    />
-                  </div>
+                  <div className="mt-[20px] flex flex-row items-start justify-between text-[14px] font-[600]">
+                    <div className="flex flex-row items-start font-[400]">
+                      <PriceWithIcon
+                        priceValue={tradingData && tradingData.vammPrice ? tradingData.vammPrice.toFixed(2) : '-.--'}
+                        className={`${!tradingData || isMarketDataUpdating ? 'flash' : ''} !font-[400]`}
+                      />
+                      &nbsp; / &nbsp;
+                      <PriceWithIcon
+                        priceValue={tradingData && tradingData.oraclePrice ? tradingData.oraclePrice.toFixed(2) : '-.--'}
+                        className={`${!tradingData || isMarketDataUpdating ? 'flash' : ''} !font-[400] !text-mediumEmphasis`}
+                      />
+                    </div>
 
-                  <div>
-                    <PriceWithIcon
-                      priceValue={`${isPriceGapPositive ? '+' : ''}${priceGap.toFixed(2)} (${
-                        isPriceGapPositive ? '+' : ''
-                      }${priceGapPercentage.toFixed(2)}%)`}
-                      className={`${!tradingData || isMarketDataUpdating ? 'flash' : ''} !font-[400]`}
-                    />
+                    <div>
+                      <PriceWithIcon
+                        priceValue={`${isPriceGapPositive ? '+' : ''}${priceGap.toFixed(2)} (${
+                          isPriceGapPositive ? '+' : ''
+                        }${priceGapPercentage.toFixed(2)}%)`}
+                        className={`${!tradingData || isMarketDataUpdating ? 'flash' : ''} !font-[400]`}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="mt-[8px]">
-                  <div
-                    className={`flex w-[70px] text-[14px] ${!tradingData || isMarketDataUpdating ? 'flash' : ''}
+                  <div className="mt-[8px]">
+                    <div
+                      className={`flex w-[70px] text-[14px] ${!tradingData || isMarketDataUpdating ? 'flash' : ''}
                     ${
                       !tradingData || !tradingData.priceChangeRatio24h
                         ? ''
@@ -122,27 +127,28 @@ export default function DisplayCollections() {
                         ? 'text-marketRed'
                         : ''
                     }`}>
-                    {tradingData && (
-                      <Image
-                        src={
-                          tradingData && tradingData.priceChangeRatio24h > 0
-                            ? '/images/components/trade/chart/polygon_pos.svg'
-                            : '/images/components/trade/chart/polygon_neg.svg'
-                        }
-                        alt=""
-                        width={16}
-                        height={16}
-                      />
-                    )}
+                      {tradingData && (
+                        <Image
+                          src={
+                            tradingData && tradingData.priceChangeRatio24h > 0
+                              ? '/images/components/trade/chart/polygon_pos.svg'
+                              : '/images/components/trade/chart/polygon_neg.svg'
+                          }
+                          alt=""
+                          width={16}
+                          height={16}
+                        />
+                      )}
 
-                    <span className="ml-1">{tradingData ? `${Math.abs(tradingData.priceChangeRatio24h.toFixed(2))}%` : '-.-- %'}</span>
+                      <span className="ml-1">{tradingData ? `${Math.abs(tradingData.priceChangeRatio24h.toFixed(2))}%` : '-.-- %'}</span>
+                    </div>
                   </div>
                 </div>
+                <div className="h-[1px] w-full bg-[#2E4371]" />
               </div>
-              <div className="h-[1px] w-full bg-[#2E4371]" />
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
       <div
         className="absolute bottom-0 flex h-[50px] w-full items-center justify-center
