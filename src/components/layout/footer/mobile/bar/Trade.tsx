3393d68a22ec7bdcc82d3ccd0cae6ed6c56c8d8a
Edward <edward@tribe3.xyz>
2023-07-22 04:32:44 +0900

    fix: injected connectors

--- src/components/layout/footer/mobile/bar/Trade.tsx
+++ src/components/layout/footer/mobile/bar/Trade.tsx
@@ -1,17 +1,16 @@
-/* eslint-disable no-alert */
 import React from 'react';
 import { ThreeDots } from 'react-loader-spinner';
 // import Sidebar from '@/components/layout/footer/mobile/Sidebar';
 import { useStore as useNanostore } from '@nanostores/react';
-// import { useWeb3Modal } from '@web3modal/react';
+import { useWeb3Modal } from '@web3modal/react';
 import { DEFAULT_CHAIN } from '@/const/supportedChains';
 import { useConnect, useSwitchNetwork } from 'wagmi';
 import { $userIsConnected, $userIsConnecting, $userIsWrongNetwork, $userWethBalance } from '@/stores/user';
 import { $isShowTradingMobile } from '@/stores/trading';
-import { $isShowMobileModal, $isShowMobileTokenModal, $showSwitchNetworkErrorModal } from '@/stores/modal';
+import { $isShowMobileModal, $isShowMobileTokenModal } from '@/stores/modal';
 
 function MobileTradeFooterInfo() {
-  // const { open } = useWeb3Modal();
+  const { open } = useWeb3Modal();
   const { switchNetwork } = useSwitchNetwork();
   const isConnected = useNanostore($userIsConnected);
   const isConnecting = useNanostore($userIsConnecting);
@@ -19,47 +18,27 @@ function MobileTradeFooterInfo() {
   const isWrongNetwork = useNanostore($userIsWrongNetwork);
   const isWethCollected = wethBalance !== 0;
   const showWethBalanceLabel = !isConnected ? '' : isWrongNetwork ? '-.-- WETH' : `${Number(wethBalance).toFixed(2)} WETH`;
-  const { /* connect, */ connectors } = useConnect();
+  const { connect, connectors } = useConnect();
 
   const onClickBottomButton = async () => {
     if (!isConnected) {
-      // console.log(connectors);
-      // const connector0 = connectors[0];
-      // const connector1 = connectors[1];
-      // alert(connector0);
-      // alert(connector0.id);
-      // alert(connector0.name);
-      // alert(connector1);
-      // alert(connector1.id);
-      // alert(connector1.name);
-      // const connector = connectors[1];
-      // connect({ connector });
-
-      // let isInjected = false;
+      let isInjected = false;
 
       for (let i = 0; i < connectors.length; i += 1) {
         const connector = connectors[i];
-        alert(connector.id);
-        alert(connector.name);
-        // if (connector?.id.includes('injected')) {
-        //   connect({ connector });
-        //   isInjected = true;
-        // }
+        if (connector?.id.includes('injected')) {
+          connect({ connector });
+          isInjected = true;
+        }
       }
 
-      // if (!isInjected) {
-      //   open();
-      // }
-
-      return;
+      if (!isInjected) {
+        open();
+      }
     }
 
-    if (isWrongNetwork) {
-      if (switchNetwork) {
-        switchNetwork(DEFAULT_CHAIN.id);
-      } else {
-        $showSwitchNetworkErrorModal.set(true);
-      }
+    if (isWrongNetwork && switchNetwork) {
+      switchNetwork(DEFAULT_CHAIN.id);
       return;
     }
 
@@ -83,7 +62,11 @@ function MobileTradeFooterInfo() {
         {isConnecting ? (
           <ThreeDots ariaLabel="loading-indicator" height={50} width={50} color="white" />
         ) : !isConnected ? (
-          <div>{connectors[1].ready ? 't' : 'f'}</div>
+          <>
+            Connect
+            <br />
+            Wallet
+          </>
         ) : isWrongNetwork ? (
           <>
             Switch to <br /> Arbitrum
