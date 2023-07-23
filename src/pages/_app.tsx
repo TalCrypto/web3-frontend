bd759a346990ce3eda5a5f31a8d397f3f9347d86
Edward <edward@tribe3.xyz>
2023-07-23 17:54:06 +0900

    fix: change shimdisconnect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,7 +43,7 @@ const wagmiConfig = createConfig({
       chains: CHAINS,
       options: {
         name: 'Injected',
-        shimDisconnect: false
+        shimDisconnect: true
       }
     })
   ],
