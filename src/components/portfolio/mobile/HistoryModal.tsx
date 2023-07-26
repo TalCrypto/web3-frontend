696fc23704db8fcbb700ace29ad0b7c0a6d67826
agamtribe3 <agam@tribe3.xyz>
2023-07-26 19:54:53 +0700

    fix: isNaN again

--- src/components/portfolio/mobile/HistoryModal.tsx
+++ src/components/portfolio/mobile/HistoryModal.tsx
@@ -210,7 +210,7 @@ const HistoryModal = () => {
                     className={`${Number(selectedBalance) > 0 ? 'text-marketGreen' : Number(selectedBalance) < 0 ? 'text-marketRed' : ''}
                       mt-[6px] text-[20px] font-semibold`}
                     priceValue={`${Number(selectedBalance) > 0 ? '+' : ''}${
-                      Number(selectedBalance) === 0 ? '-.--' : Number(selectedBalance).toFixed(4)
+                      Number(selectedBalance) === 0 || Number.isNaN(selectedBalance) ? '-.--' : Number(selectedBalance).toFixed(4)
                     }`}
                   />
                 </div>
