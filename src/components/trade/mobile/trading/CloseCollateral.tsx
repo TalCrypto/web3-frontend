e9f1cb413e9f0c9a316ae05e34167f48394a5103
agamtribe3 <agam@tribe3.xyz>
2023-07-27 11:58:01 +0700

    remove alert

--- src/components/trade/mobile/trading/CloseCollateral.tsx
+++ src/components/trade/mobile/trading/CloseCollateral.tsx
@@ -422,8 +422,6 @@ export default function CloseCollateral() {
   const handleError = useCallback((error: Error | null, isPrepareError: boolean) => {
     setIsPending(false);
 
-    alert('error');
-    alert(error);
     setPrepareTextErrorMessage(error && isPrepareError ? formatError(error.message) : null);
     setWriteTextErrorMessage(error && !isPrepareError ? formatError(error.message) : null);
   }, []);
