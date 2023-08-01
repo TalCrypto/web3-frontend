dce1b5ece95c12c78f95ef0c9777d70438385eb1
agamtribe3 <agam@tribe3.xyz>
2023-08-01 10:19:57 +0700

    feat: chart setting save to store

--- src/stores/chart.ts
+++ src/stores/chart.ts
@@ -1,4 +1,20 @@
-import { atom } from 'nanostores';
+// import { atom } from 'nanostores';
+import { persistentAtom } from '@nanostores/persistent';
 
-export const $isSettingVammOn = atom(true);
-export const $isSettingOracleOn = atom(true);
+export const $isSettingVammOn = persistentAtom<boolean>('chartSettingVammOn', true, {
+  encode(val) {
+    return val ? 'true' : 'false';
+  },
+  decode(val) {
+    return val === 'true';
+  }
+});
+
+export const $isSettingOracleOn = persistentAtom<boolean>('chartSettingOracleOn', true, {
+  encode(val) {
+    return val ? 'true' : 'false';
+  },
+  decode(val) {
+    return val === 'true';
+  }
+});
